57b9a802c527167d51643215c26d561b5fb3a2aa
AlexisW <alexiswright@ufl.edu>
2023-01-22 22:55:16 -0500

    commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function greet() {
+   
+    var name = prompt ("What is your name?", "");
+    alert("Hello, " + name);
+    
+    var age = prompt (name + ", how old are you?", "");
+    console.log (parseInt(age));
+
+    var birthday = confirm("Have you had a birthday yet this year?");
+
+    if ( birthday == true)
+    {
+        alert ("You were born in " + ((2023) - age))
+    }else{
+        alert("You were born in " + ((2023) - age - 1))
+    }
+}
